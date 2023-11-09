eb0727c181ae4dde1e947131c601fd3e56b46665
Waffle <140558721+WaffleBender@users.noreply.github.com>
2023-11-08 23:21:58 -0500

    Added code

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+class Main {
+    public static void main(String []args) {
+        Random rand = new Random();
+
+        //Random number generator (0=Rock, 1+Paper, and 2=Scissors)
+        int Computer = rand.nextInt(3);
+
+        //Player select ui (0=Rock, 1+Paper, and 2=Scissors)
+        String[] options = new String[] {"Rock", "Paper", "Scissors"};
+            int Player = JOptionPane.showOptionDialog(null, "The computer has chosen. Now it's your turn", "Shoot",
+            JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
+            null, options, options[0]);
+
+        switch (Player) {
+
+            //Rock
+            case 0: switch (Computer) {
+                    case 0: JOptionPane.showMessageDialog(null, "Computer chose rock, it's a draw.");
+                        break;
+                    case 1: JOptionPane.showMessageDialog(null, "Computer chose paper. you lost");
+                        break;
+                    default: JOptionPane.showMessageDialog(null, "Computer chose scissors, you won!");
+            };
+                break;
+
+            //Paper
+            case 1: switch (Computer) {
+                    case 0: JOptionPane.showMessageDialog(null, "Computer chose rock, you won :)");
+                        break;
+                    case 1: JOptionPane.showMessageDialog(null, "Computer chose paper. it's a draw :|");
+                        break;
+                    default: JOptionPane.showMessageDialog(null, "Computer chose scissors, you lost :(");
+            };
+                break;
+
+            //Scissors
+            case 2: switch (Computer) {
+                    case 0: JOptionPane.showMessageDialog(null, "Computer chose rock, you lost :(");
+                        break;
+                    case 1: JOptionPane.showMessageDialog(null, "Computer chose paper. you won :)");
+                        break;
+                    default: JOptionPane.showMessageDialog(null, "Computer chose scissors, it's a draw :|");
+            }
+                break;
+
+            default: 
+        }
+    }
+}
